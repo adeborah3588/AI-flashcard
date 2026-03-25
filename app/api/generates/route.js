6dd975aff76ed158fafbec23379a57766fa5cc54
Deborah Ifeoluwa <ayekudeborah18@gmail.com>
2026-03-25 14:05:45 +0100

    FIXING MODEL

--- app/api/generates/route.js
+++ app/api/generates/route.js
@@ -48,7 +48,7 @@ export async function POST(req) {
       { role: "system", content: systemPrompt },
       { role: "user", content: data },
     ],
-    model: "llama3-8b-8192", // Specify the model to use
+    model: "llama-3.3-70b-versatile", // Specify the model to use
     response_format: { type: "json_object" },
   });
 
